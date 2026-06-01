af5953a0888d73852655cc87710e9d432c7a0313
Methi Lyle <methilyl@pr-a6.kzn.21-school.ru>
2026-06-01 18:18:00 +0300

    all

--- EXAM/1.2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int x1, x2;
-    scanf("%d %d", &x1, &x2);
-    printf("%d", x1 * x2);
-    return 0;
-}
--- EXAM/2.4.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("C:\\Windows\\System32\\notepad.exe");
-    return 0;
-}
